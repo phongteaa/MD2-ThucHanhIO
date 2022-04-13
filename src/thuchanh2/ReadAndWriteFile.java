2eabc5655b059ab60eba81b42323ea2b538cb76c
phongteaa <phongteaa@gmail.com>
2022-04-13 08:44:24 +0700

    first

---
+++ src/thuchanh2/ReadAndWriteFile.java
@@ -0,0 +1,59 @@
+package thuchanh2;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadAndWriteFile {
+    public List<Integer> readFile(String filePath) {
+        List<Integer> numbers = new ArrayList<>();
+        try {
+            File file = new File(filePath);
+
+            if (!file.exists()) {
+                throw new FileNotFoundException();
+            }
+
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                numbers.add(Integer.parseInt(line));
+            }
+            bufferedReader.close();
+        } catch (Exception e) {
+            System.out.println(e);
+            System.err.println("File does not exist or content has error.");
+
+        }
+        return numbers;
+    }
+
+    public void writeFile(String filePath, int max) {
+        try {
+            FileWriter writer = new FileWriter(filePath, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(writer);
+            bufferedWriter.write("Max value: " + max);
+            System.out.println();
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int findMax(List<Integer> numbers) {
+        int max = numbers.get(0);
+        for (int i = 0; i < numbers.size(); i++) {
+            if (max < numbers.get(i)) {
+                max = numbers.get(i);
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        ReadAndWriteFile readAndWriteFile = new ReadAndWriteFile();
+        List<Integer> numbers = readAndWriteFile.readFile("text.text");
+        int maxValue = findMax(numbers);
+        readAndWriteFile.writeFile("result.text", maxValue);
+    }
+}
